24371fe28dfc1ade66cf5587adfac252f9682ee9
diazgabe <45862947+diazgabe@users.noreply.github.com>
2021-06-25 21:23:35 -0400

    integrated firebase into application, created sign in page with sign in component and sign in with google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,45 +1,65 @@
-import React from 'react'
-import FormInput from '../form-input/form-input.component';
-import CustomButton from '../custom-button/custom-button.component';
-import './sign-in.styles.scss'
+import React from "react";
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+import "./sign-in.styles.scss";
+import { signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            email: '',
-            password: ''
-        }
-    }
-
-    handleSubmit = event => {
-        event.preventDefault();
-
-        this.setState({ email: '', password: '' });
+    this.state = {
+      email: "",
+      password: "",
     };
+  }
 
-    handleChange = event => {
-        const { value, name } = event.target;
+  handleSubmit = (event) => {
+    event.preventDefault();
 
-        this.setState({ [name]: value });
-    };
+    this.setState({ email: "", password: "" });
+  };
 
-    render() {
-        return (
-            <div className='sign-in'>
-                <h2>I already have an account</h2>
-                <span>Sign in with your email and password</span>
+  handleChange = (event) => {
+    const { value, name } = event.target;
 
-                <form onSubmit={this.handleSubmit}>
-                    <FormInput name="email" type="email" value={this.state.email} label="email" handleChange={this.handleChange} required />
-                    <FormInput name="password" type="password" value={this.state.password} label="password" handleChange={this.handleChange} required />
+    this.setState({ [name]: value });
+  };
 
-                    <CustomButton type="submit" >Sign In</CustomButton>
-                </form>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name="email"
+            type="email"
+            value={this.state.email}
+            label="email"
+            handleChange={this.handleChange}
+            required
+          />
+          <FormInput
+            name="password"
+            type="password"
+            value={this.state.password}
+            label="password"
+            handleChange={this.handleChange}
+            required
+          />
+          <div className="buttons">
+            <CustomButton type="submit">Sign In</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              {" "}
+              Sign in with Google{" "}
+            </CustomButton>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyC5eMNFoPw4N1vSIZTrDS1_pei8jL0l0PA",
+    authDomain: "crwn-db-3c447.firebaseapp.com",
+    projectId: "crwn-db-3c447",
+    storageBucket: "crwn-db-3c447.appspot.com",
+    messagingSenderId: "259475983392",
+    appId: "1:259475983392:web:778b0f00d9ae9c08974045"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
