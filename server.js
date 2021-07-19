f40e46df3044ef4900f8c345a80f824d75fe2eb1
diazgabe <45862947+diazgabe@users.noreply.github.com>
2021-07-19 18:41:06 -0400

    Undo last change which broke heroku app.

--- server.js
+++ server.js
@@ -11,13 +11,12 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(compression);
-  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", function (req, res) {
@@ -27,7 +26,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.listen(port, (error) => {
   if (error) throw error;
-  console.log("Server is running on port " + port);
+  console.log("Server running on port " + port);
 });
 
 app.get("/service-worker.js", (req, res) => {
